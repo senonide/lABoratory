9bc56d5d6d79c6f296b0896fefeeeac179c075f3
Sergio <senonide@gmail.com>
2022-03-31 19:51:08 +0200

    Description field added

--- lABoratoryAPI/models/experiment.go
+++ lABoratoryAPI/models/experiment.go
@@ -3,6 +3,7 @@ package models
 type Experiment struct {
 	Id          string       `bson:"_id,omitempty" json:"id,omitempty"`
 	Name        string       `json:"name" binding:"required"`
+	Description string       `json:"description"`
 	Assignments []Assignment `json:"assignments" binding:"required"`
 	Owner       User         `json:"owner,omitempty"`
 }
