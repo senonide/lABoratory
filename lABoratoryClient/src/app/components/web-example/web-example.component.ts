d734c7b662236cf3d009b4a23b07e8dad44ee32b
Sergio Nonide <senonide@gmail.com>
2022-05-17 09:18:12 +0200

    Experiment example

--- lABoratoryClient/src/app/components/web-example/web-example.component.ts
+++ lABoratoryClient/src/app/components/web-example/web-example.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Config } from 'src/app/config/config';
+import { Customer } from 'src/app/models/customer.model';
 
 @Component({
   selector: 'web-example',
@@ -8,11 +11,64 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class WebExample {
 
-  public price: String = "0"
+  public price: String = "0";
+
+  public experimentId: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MjgzNDg2OGFhNmE1M2U2YzJlZTJmOTMifQ.xpBvvZd1S8Dz08SKD-WxGKlIIMft6fQv5k2B-D2okQQ";
+
+  public assignment: String = "";
+
+  public logged: boolean = false;
+
+  constructor(private http: HttpClient) {}
 
   usernameForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
   });
+
+  loadNewUser(): void {
+    var experimentKey: String = this.usernameForm.value.username;
+    var response = this.getAssignment(this.experimentId, experimentKey);
+      if(response==null){
+          return
+      } else {
+        response.subscribe({
+            next: (value) => {
+                this.assignment = value.assignment;
+                this.getPriceFromAssignment(this.assignment);
+                this.logged = true;
+            },
+            error: () => {
+              console.log("ERROR")
+            }
+        });
+      }
+  }
+
+  getAssignment(experimentId: String, experimentKey: String) {
+    return this.http.get<Customer>(Config.apiUrl + '/assignment' + '/' + experimentId + '/' + experimentKey);
+  }
+
+  getPriceFromAssignment(assignment: String): void {
+    switch (assignment) {
+      case "c":
+        this.price = "500";
+        break;
+      case "a1":
+        this.price = "499";
+        break;
+      case "a2":
+        this.price = "450";
+        break;
+      case "a3":
+        this.price = "489";
+        break;
+      case "a4":
+        this.price = "510";
+        break;
+    }
+  }
+
+  
 
   
 }
